79dd7f038082c791e3d7a7af580b5885590e7423
Emanuel VF <emanuelvillafernandez@gmail.com>
2020-05-01 17:41:57 -0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Emanuel on 01/05/2020.
+//  Copyright © 2020 Emanuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
