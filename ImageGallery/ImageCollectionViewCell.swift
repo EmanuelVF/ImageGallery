bb782257dde4810527c3224840756e08bf7dc42c
Emanuel VF <emanuelvillafernandez@gmail.com>
2020-05-01 18:12:47 -0300

    ViewControllers added TableViewController added Utilities file added ImageGallery added

---
+++ ImageGallery/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Emanuel on 01/05/2020.
+//  Copyright © 2020 Emanuel. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ImageGallery/ImageGalleryView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageGalleryView.swift
+//  ImageGallery
+//
+//  Created by Emanuel on 01/05/2020.
+//  Copyright © 2020 Emanuel. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
