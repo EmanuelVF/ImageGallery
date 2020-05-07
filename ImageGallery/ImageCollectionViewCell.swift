48e049a9603453f5c8eb5c0838d1b5e112501c4b
Emanuel VF <emanuelvillafernandez@gmail.com>
2020-05-06 21:29:43 -0300

    Deleting prints

--- ImageGallery/ImageCollectionViewCell.swift
+++ ImageGallery/ImageCollectionViewCell.swift
@@ -40,14 +40,12 @@ class ImageCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     private func fetchImage(){
-        print("A buscar la foto!")
             if let url = imageURL{
                 spinner.startAnimating()
                 DispatchQueue.global(qos:.userInitiated).async { [weak self] in  // This weak is added if self doesn't exist when the image is loaded. It is not related with memory cycles
                     let urlContents = try? Data(contentsOf: url.imageURL) // ? is added to handle the throws. If I am interested in the error, then the try catch should be done
                     DispatchQueue.main.async {
                         if let imageData = urlContents, url == self?.imageURL{ // to keep updated url
-                            print("Traje la foto otra vez!")
                             self?.image = UIImage(data: imageData)
                             self?.aspectRatio = (self!.image?.size.width ?? 1)/(self!.image?.size.height ?? 1)
                             for index in ImageGallery.Gallery.indices{
